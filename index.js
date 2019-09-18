0e80d9ad040e351b60d0978a2bcab162802acabd
Pål Edman <pal.edman@gmail.com>
2019-09-18 09:45:07 +0200

    replace request with got

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const request = require("request");
+const request = require("got");
 const VError = require("verror");
 const AsyncCache = require("exp-asynccache");
 const clone = require("clone");
@@ -160,6 +160,7 @@ function buildFetch(behavior) {
         json: contentType === "json",
         agent: keepAliveAgent,
         followRedirect: false,
+        retry: false,
         method: httpMethod,
         timeout: explicitTimeout || timeout,
         headers: headers
@@ -177,7 +178,6 @@ function buildFetch(behavior) {
         headers: options.headers
       };
       if (onRequestInit && !onRequestInit.called) {
-
         onRequestInit(passOptions, cacheKey);
       }
 
@@ -186,19 +186,21 @@ function buildFetch(behavior) {
         resolveFunction(err, content, maxAge);
       }
 
-      request(options, function (err, res, content) {
-        if (err) return resolvedCallback(new VError(err, "Fetching error for: %j", url));
-        if (isRedirect(res)) return handleRedirect(url, cacheKey, res, content, resolvedCallback);
-
-        if (res.statusCode === 404) {
-          return handleNotFound(url, cacheKey, res, content, resolvedCallback);
-        } else if (res.statusCode > 299) {
-          return handleError(url, cacheKey, res, content, resolvedCallback);
-        }
-
-        return parseResponse(content, contentType, function (_, transformed) {
+      return request(options).then((res) => {
+        if (isRedirect(res)) return handleRedirect(url, cacheKey, res, res.body, resolvedCallback);
+        return parseResponse(res.body, contentType, function (_, transformed) {
           return handleSuccess(url, cacheKey, res, transformed, resolvedCallback);
         });
+      }).catch((err) => {
+        if (err.statusCode === 404) {
+          return handleNotFound(url, cacheKey, err, err.body, resolvedCallback);
+        } else if (err.statusCode > 299) {
+          return handleError(url, cacheKey, err, err.body, resolvedCallback);
+        } else if (err instanceof request.TimeoutError) {
+          return resolvedCallback(new VError("ESOCKETTIMEDOUT"));
+        }
+
+        resolvedCallback(err);
       });
     }, function (err, response) {
       if (followRedirect && isRedirect(response)) {
